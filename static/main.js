2123a67edfef7ffd77f8c22e4407d2834d312c51
Silke <silke@slxh.eu>
2018-02-26 13:49:59 +0100

    Perform linting of the JavaScript code
    - Add JSHint configuration file
    - Rename 'length' constant
    - Move time formatting to formatTime
    - Move hash parsing to parseLocationHash
    - Move hash updating to updateLocationHash
    - Move random password generation to generatePassword
    - Create settings objects for list behaviour tweaking
    - Appease all other minor JSHint issues

--- static/main.js
+++ static/main.js
@@ -18,15 +18,19 @@
 
 const indexFile = "../index.json";
 const listDir = "../lists/";
-const language = document.querySelector('html').getAttribute('lang');
+const language = document.querySelector("html").getAttribute("lang");
 const defaultLength = 4;
 const hashSpeed = 5; // In 10^6 attempts/second
 const list = document.getElementById("list");
-const length = document.getElementById("password-length");
+const passwordLength = document.getElementById("password-length");
 
 // Password list
 let passwords = [];
 let defaultList = "";
+
+// Password settings
+const defaultPasswordSettings   = { joinChar: " ", lowerCase: true };
+const shortListPasswordSettings = { joinChar: "", lowerCase: false };
 
 // Time units
 const secondsHour = 3600;
@@ -39,39 +43,63 @@ let units = {
     "eternity": "∞ s"
 };
 
-// Initialise the password generator.
-try {
-    init();
-}
-catch(error) {
-    document.getElementById("error").style.display = "block";
-    throw error;
-}
-
 // Set the inner HTML for an element
 function setHTML(selector, html) {
-    let list = document.querySelectorAll(selector);
-    for (let e of list) e.innerHTML = html;
-}
-
-// Generate a password from the loaded list
-function generate() {
     "use strict";
 
-    // Required password length
-    let minLength = length.value * Math.log(passwords.length) / Math.log(26);
-    let joinChar = " ";
-    let lowerCase = true;
+    let list = document.querySelectorAll(selector);
+    for (let e of list) {
+        e.innerHTML = html;
+    }
+}
 
-    // Small lists have different behaviour
-    if (passwords.length < 1000 || passwords[0] === "😀") {
-        minLength = 0;
-        joinChar = "";
-        lowerCase = false;
+// Format time to a human readable format.
+function formatTime(time) {
+    "use strict";
+
+    let text = "";
+    if (time >= 0 && time < 2 * secondsDay) {
+        text = Math.round(time / secondsHour) + " " + units.hour;
+    } else if (time < 2 * secondsYear) {
+        text = Math.round(time / secondsDay) + " " + units.day;
+    } else if (time < secondsYear * Math.pow(10, 9)) {
+        text = Math.round(time / secondsYear) + " " + units.year;
+    } else {
+        text = units.eternity;
     }
 
+    return text;
+}
+
+
+// Get settings from the location hash
+function parseLocationHash() {
+    "use strict";
+    const hash = window.location.hash.substring(1).split(";");
+    return {
+        list: hash[0],
+        length: Number(hash[1])
+    };
+}
+
+// Update the window location hash
+function updateLocationHash() {
+    "use strict";
+    if (window.location.hash ||
+        list.value !== defaultList ||
+        Number(passwordLength.value) !== defaultLength)
+    {
+        window.location.hash = "#" + list.value + ";" + passwordLength.value;
+        document.querySelector("a.generate.button").href = window.location.hash;
+    }
+}
+
+// Generate a random password from the loaded list
+function generatePassword(length, lowerCase) {
+    "use strict";
+
     // Array for password generation
-    const random = new Uint32Array(length.value);
+    const random = new Uint32Array(passwordLength.value);
     let gen = [];
 
     // Fill the array with random values
@@ -90,14 +118,35 @@ function generate() {
         gen.push(w);
     }
 
+    return gen;
+}
+
+// Generate a password from the loaded list and show it.
+function generate() {
+    "use strict";
+
+    // Required password passwordLength
+    let minLength = passwordLength.value * Math.log(passwords.length) / Math.log(26);
+    let settings  = defaultPasswordSettings;
+
+    // Small lists have different behaviour
+    if (passwords.length < 1000 || passwords[0] === "😀") {
+        minLength = 0;
+        settings  = shortListPasswordSettings;
+    }
+
+    // Generate a random password
+    const gen = generatePassword(passwordLength.value, settings.lowerCase);
+
     // Check if the password has as much entropy as the individual letters
     if (gen.join("").length >= minLength) {
         // Display the password
-        setHTML("h1#password", gen.join(joinChar));
+        setHTML("h1#password", gen.join(settings.joinChar));
     } else {
         // Try again
         console.log(
-            "Not enough entropy, " + minLength + " characters required: " + gen.join(joinChar)
+            "Not enough entropy, " + minLength + " characters required: " +
+            gen.join(settings.joinChar)
         );
         generate();
     }
@@ -107,33 +156,18 @@ function generate() {
 function update() {
     "use strict";
 
-    const space = Math.pow(passwords.length, length.value);
+    const space = Math.pow(passwords.length, passwordLength.value);
     const bits = Math.log(space) / Math.log(2);
     const time = space / 2 / hashSpeed / Math.pow(10, 6);
-    let text = "";
-
-    // Make time readable
-    if (time >= 0 && time < 2 * secondsDay) {
-        text = Math.round(time / secondsHour) + " " + units.hour;
-    } else if (time < 2 * secondsYear) {
-        text = Math.round(time / secondsDay) + " " + units.day;
-    } else if (time < secondsYear * Math.pow(10, 9)) {
-        text = Math.round(time / secondsYear) + " " + units.year;
-    } else {
-        text = units.eternity;
-    }
 
     // Show the results in the text
     setHTML(".wordCount", passwords.length);
     setHTML(".entropyBits", Math.round(bits));
-    setHTML(".entropyYears", text);
+    setHTML(".entropyYears", formatTime(time));
     setHTML(".hashSpeed", hashSpeed);
 
     // Update the URL hash
-    if (list.value !== defaultList || Number(length.value) !== defaultLength) {
-        window.location.hash = '#' + list.value + ';' + length.value;
-        document.querySelector("a.generate.button").href = window.location.hash
-    }
+    updateLocationHash();
 
     // Generate a new password
     generate();
@@ -161,14 +195,15 @@ function appendList(name, data) {
     let list = document.getElementById("list");
 
     // Add description option
-    let option = new Option(data['_description']);
+    let option = new Option(data._description);
     option.disabled = true;
     list.add(option);
 
     // Add other options
     for (let l in data) {
-        if (l.substring(0, 1) === "_") continue;
-        list.add(new Option(data[l], name + "/" + l));
+        if (data.hasOwnProperty(l) && l.substring(0, 1) !== "_") {
+            list.add(new Option(data[l], name + "/" + l));
+        }
     }
 }
 
@@ -176,11 +211,11 @@ function appendList(name, data) {
 function init() {
     "use strict";
 
-    // Set callbacks for list/length changes
+    // Set callbacks for list/passwordLength changes
     list.onchange = () => {
         loadPasswords(list.value);
     };
-    length.onchange = () => {
+    passwordLength.onchange = () => {
         update();
     };
 
@@ -198,21 +233,30 @@ function init() {
             // Save units
             units = data[language].units;
 
-            // Set list and length to defaults
+            return lists;
+        })
+        .then((lists) => {
+            // Set list and passwordLength to defaults
             defaultList = language + "/" + lists.default;
-            let listName = defaultList;
-            let pwLength = defaultLength;
+            let conf = {list: defaultList, length: defaultLength};
 
-            // Override list/length from location
+            // Override list/passwordLength from location
             if (window.location.hash) {
-                const hash = window.location.hash.substring(1).split(';');
-                listName = hash[0];
-                pwLength = Number(hash[1]);
+                conf = parseLocationHash();
             }
 
             // Set the selectors to the correct values and load the list.
-            list.value = listName;
-            length.value = pwLength;
-            loadPasswords(listName);
+            list.value = conf.list;
+            passwordLength.value = conf.length;
+            loadPasswords(conf.list);
         });
+}
+
+// Initialise the password generator.
+try {
+    init();
+}
+catch(error) {
+    document.getElementById("error").style.display = "block";
+    throw error;
 }
